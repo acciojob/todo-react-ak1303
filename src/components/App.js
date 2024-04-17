0248e0042512b9ea297433a4ff18e6b15d4c720e
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-04-17 18:33:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,16 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+  const categories = ['All','breakfast','lunch','shakes'];
+    const [category,setCategory]=useState("All");
+    const selectedDishes = menu.filter(dish=> category==='All' || dish.category===category);
+    return (
+    <>
+        <h1 style={{textAlign:"center"}}>Our Menu</h1>
+        <Filter categories={categories} category={category} setCategory={setCategory}/>
+        <Dishes selectedDishes={selectedDishes}/>
+    </>
+    )
 }
 
 export default App
